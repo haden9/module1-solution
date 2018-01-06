81d2fd973d3802d6f88b5af67ac19eec280a7da8
haden9 <haden9@gmail.com>
2018-01-05 19:59:12 -0600

    Add optional functionality

--- app.js
+++ app.js
@@ -8,38 +8,69 @@
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
+
         $scope.lunchString = '';
         $scope.message = '';
+        $scope.textColor = { 'color': 'red' };
+        $scope.borderColor = { 'border': 'lightgrey 1px solid' };
 
-        $scope.checkQuantity = function () {
+        $scope.updateUI = function () {
+
+            checkQuantity();
+            changeTextColor();
+            changeBorderColor();
+        }
+
+         function checkQuantity () {
 
             if ($scope.lunchString.length == 0) {
                 $scope.message = 'Please enter data first';
-            } else if (stringSplitter($scope.lunchString).length <= 3) {
+            } else if (splitString($scope.lunchString).length <= 3) {
                 $scope.message = 'Enjoy!';
             } else {
                 $scope.message = 'Too much!';
             }
         };
 
-        function stringSplitter(string) {
-            var result = emptyValuesRemover(string.split(','));
-            // putting this console here to witness trimification and splitification
-            console.log(result);
+        function splitString(string) {
+
+            var result = removeEmptyValues(string.split(','));
+
             return result;
         }
 
-        function emptyValuesRemover(array) {
+        function removeEmptyValues(array) {
             var nonEmptyValuesArray = [];
 
             for (var i = 0; i < array.length; i ++) {
-                // needed to add the trim here since this is not an angular model declaration
                 if (array[i].trim().length != 0) {
                     nonEmptyValuesArray.push(array[i]);
                 }
             }
 
             return nonEmptyValuesArray;
+        }
+
+        function changeTextColor() {
+
+            if (validateMessage()) {
+                $scope.textColor = { 'color': 'green' };
+            } else {
+                $scope.textColor = { 'color': 'red' };
+            }
+        }
+
+        function changeBorderColor() {
+
+            if (validateMessage()) {
+                $scope.borderColor = { 'border': 'green 1px solid' };
+            } else {
+                $scope.borderColor = { 'border': 'red 1px solid' };
+            }
+        }
+
+        function validateMessage() {
+            return $scope.message === 'Enjoy!' || $scope.message === 'Too much!';
         }
 
     };
